bdcc542347e51acc02f0f7a7fc6c47e8c1035ef9
BPhilipose <BensonPhilipose@gmail.com>
2020-08-17 13:22:11 -0500

    First Commit

---
+++ django_main/django_main/urls.py
@@ -0,0 +1,10 @@
+"""django_main URL Configuration
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls'))
+    
+]
